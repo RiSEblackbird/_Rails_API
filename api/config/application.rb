06c161c5ca0955e5da2d70bd9f71fc2684890e80
SELA <RiSEblackbird@users.noreply.github.com>
2020-09-27 18:25:09 +0900

    api/config/application.rb : 設定を記述する位置を修正

--- api/config/application.rb
+++ api/config/application.rb
@@ -33,9 +33,9 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
-end
 
-config.generators do |g|
-  g.test_framework :rspec
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+  end
 end
